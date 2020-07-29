36984e37d3a9248272ea20dc9c4949b900f4c8c7
Julio Cesar Arbieto <arbieto.jc@gmail.com>
2020-07-29 12:05:57 -0500

    microservicios inicializando

---
+++ CONTINER.API.MANAGER/CONTINER.API.MANAGER.Account/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using CONTINER.API.MANAGER.Account.DTO;
+using CONTINER.API.MANAGER.Account.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CONTINER.API.MANAGER.Account.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IServiceAccount _services;
+        public AccountController(IServiceAccount services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_services.GetAll());
+        }
+
+        [Authorize]
+        [HttpPost("Deposit")]
+        public IActionResult Deposit([FromBody] AccountRequest request)
+        {
+            var result = _services.GetAll().FirstOrDefault(x => x.IdAccount == request.IdAccount);
+            //.FirstOrDefault();
+            Model.Account account = new Model.Account()
+            {
+                IdAccount = request.IdAccount,
+                IdCustomer = result.IdCustomer,
+                TotalAmount = result.TotalAmount + request.Amount,
+                Customer = result.Customer
+            };
+            _services.Deposit(account);
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPost("Withdrawal/")]
+        public IActionResult Withdrawal([FromBody] AccountRequest request)
+        {
+            var result = _services.GetAll().Where(x => x.IdAccount == request.IdAccount)
+                .FirstOrDefault();
+            Model.Account account = new Model.Account()
+            {
+                IdAccount = request.IdAccount,
+                IdCustomer = result.IdCustomer,
+                TotalAmount = result.TotalAmount - request.Amount,
+                Customer = result.Customer
+            };
+            _services.Withdrawal(account);
+            return Ok();
+        }
+
+    }
+}
---
+++ CONTINER.API.MANAGER/CONTINER.API.MANAGER.Security/Startup.cs
@@ -0,0 +1,61 @@
+﻿using CONTINER.API.MANAGER.Cross.Jwt.Jwt;
+using CONTINER.API.MANAGER.Security.Repository;
+using CONTINER.API.MANAGER.Security.Repository.Data;
+using CONTINER.API.MANAGER.Security.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CONTINER.API.MANAGER.Security
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddJwtCustomized();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<JwtOptions>(Configuration.GetSection("jwt"));
+
+            services.AddDbContext<ContextDatabase>(
+                options =>
+                {
+                    options.UseMySQL(Configuration["mysql:cn"]);
+                });
+
+            services.AddScoped<IServiceAccess, ServiceAccess>();
+            services.AddScoped<IAccessRepository, AccessRepository>();
+            services.AddScoped<IContextDatabase, ContextDatabase>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
