67f1e54e1f84b31a54aee2bff5632c38034046a0
Julio Cesar Arbieto <arbieto.jc@gmail.com>
2020-07-29 14:00:37 -0500

    api controller -Withdrawal microservicio

---
+++ CONTINER.API.MANAGER/CONTINER.API.MANAGER.Withdrawal/Controllers/TransactionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CONTINER.API.MANAGER.Withdrawal.DTO;
+using CONTINER.API.MANAGER.Withdrawal.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CONTINER.API.MANAGER.Withdrawal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IServiceTransaction _services;
+        public TransactionController(IServiceTransaction services)
+        {
+            _services = services;
+        }
+
+
+        [HttpPost("Withdrawal")]
+        public IActionResult Withdrawal([FromBody] TransactionRequest request)
+        {
+            Model.Transaction transaction = new Model.Transaction()
+            {
+                AccountId = request.AccountId,
+                Amount = request.Amount,
+                CreationDate = DateTime.Now.ToShortDateString(),
+                Type = "Withdrawal"
+            };
+            _services.Withdrawal(transaction);
+            return Ok();
+        }
+    }
+}
